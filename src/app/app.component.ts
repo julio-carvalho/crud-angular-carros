8a02874be4ee665afd9316a6ba2ca7a681874047
Julio Carvalho <jcarvalhok3@gmail.com>
2022-06-28 17:51:10 -0300

    criação do service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { CarroService } from './services/carro.service';
+import { Carro } from './models/carro.models';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-http';
+
+  carro = {} as Carro;
+  carros: Carro[];
+
+  constructor(private carroService: CarroService) {}
+
+  ngOnInit(): void {
+    this.carroService.getCarros();
+  }
+
+  // valida se o carro já existe, se não ele cria um novo carro
+  salvaCarro(form: NgForm) {
+    if (this.carro.id !== undefined) {
+      this.carroService.updateCarro(this.carro).subscribe(() => {
+        //this.cleanForm(form);
+      });
+    } else {
+      this.carroService.salvaCarro(this.carro).subscribe(() => {
+        //this.cleanForm(form);
+      });
+    }
+  }
+
+    // Chama o service que retorna todos os carros
+    getCars() {
+      this.carroService.getCarros().subscribe((carros: Carro[]) => {
+        this.carros = carros;
+      });
+    }
+
+
 }
--- src/app/services/carro.service.ts
+++ src/app/services/carro.service.ts
@@ -1,9 +1,79 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Carro } from '../models/carro.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarroService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/cars'; // api rest fake
+
+  //incluindo o HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  //retorna todos os carros
+  getCarros(): Observable<Carro[]> {
+    return this.httpClient.get<Carro[]>(this.url)
+    .pipe(
+      retry(2),
+      catchError(this.handleError))
+  }
+
+   // retorna um carro pelo id
+   getCarroById(id: number): Observable<Carro> {
+    return this.httpClient.get<Carro>(this.url + '/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // cria um carro
+  salvaCarro(car: Carro): Observable<Carro> {
+    return this.httpClient.post<Carro>(this.url, JSON.stringify(car), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // atualiza um carro
+  updateCarro(car: Carro): Observable<Carro> {
+    return this.httpClient.put<Carro>(this.url + '/' + car.id, JSON.stringify(car), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  // deleta um carro
+  deleteCar(car: Carro) {
+    return this.httpClient.delete<Carro>(this.url + '/' + car.id, this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+   // Tratamento de erros
+   handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
 }
